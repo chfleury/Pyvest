1404f5dc703f7b19d64869e990d72c0bfd20a1f6
JPedroCh <jpaschaves@gmail.com>
2020-10-24 17:56:03 -0300

    modificação no gráfico de price  para o modelo min < investimento < max e adição de mais 2 gráficos nesse modelo porém para market_cap e change_percent

--- src/usuario_investimentos/views.py
+++ src/usuario_investimentos/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
 
+# criar um botão para cada investimento com o código e o nome de cada um
+# usar a API pra pegar o dado do dia daquele investimento
+
+investimentos_do_usuario = []
+
+def requisicao(symbol, key, nome_do_campo):
+
+    url = f'https://api.hgbrasil.com/finance/stock_price?&key={key}&symbol={symbol}&fields={nome_do_campo}'
+
+    dados = requests.get(url)
+
+    resposta_geral = dados.json()
+    somente_resultados = resposta_geral["results"]
+    valor_do_campo_desejado = somente_resultados["PETR4"]
+
+    return valor_do_campo_desejado
+
+
 
 def meus_investimentos(request):
+
+    
+
+
+
+
+
+
     return render(request, 'investimentos_do_usuario.html', {})
