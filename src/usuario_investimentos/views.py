e99f52bc084af8f3316451257447dd491539e7cd
JPedroCh <jpaschaves@gmail.com>
2020-10-26 17:32:52 -0300

    início da linked list dos investimentos

--- src/usuario_investimentos/views.py
+++ src/usuario_investimentos/views.py
@@ -1,23 +1,37 @@
 from django.shortcuts import render
 
-# criar uma linked list pros investimentos
 # criar um botão para cada investimento com o código e o nome de cada um
 # usar a API pra pegar o dado do dia daquele investimento
 
-investimentos_do_usuario = []
+class Node_Investimentos:
+    #construtor
+    def __init__(self, symbol, name):
+        "constructor to initiate this object"
+        
+        # guarda os dados
+        self.symbol = symbol
+        self.name = name
 
-def requisicao(symbol, key, nome_do_campo):
+        # guarda a referência (next item)
+        self.next = None
 
-    url = f'https://api.hgbrasil.com/finance/stock_price?&key={key}&symbol={symbol}&fields={nome_do_campo}'
+class Linked_List_Investimentos:
+    #construtor
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
-    dados = requests.get(url)
-
-    resposta_geral = dados.json()
-    somente_resultados = resposta_geral["results"]
-    valor_do_campo_desejado = somente_resultados["PETR4"]
-
-    return valor_do_campo_desejado
-
+    def output_list(self):
+        "outputs the list (the value of the node, actually)"
+        current_node = self.head
+        
+        while current_node is not None:
+            print(current_node.symbol)
+            
+            # jump to the linked node
+            current_node = current_node.next
+            
+        return current_node
 
 
 def meus_investimentos(request):
