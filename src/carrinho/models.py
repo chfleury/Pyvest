2913a76eb8a1ec32f1456583e29ea84517ff796c
Arthur Lima <arthurlindaolima544@gmail.com>
2020-11-13 19:49:36 -0300

    Adicionando investimento ao banco de dados

--- src/carrinho/models.py
+++ src/carrinho/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Investimento(models.Model):
+    symbol = models.CharField(max_length = 10)
+    name = models.CharField(max_length = 100)
+    region = models.CharField(max_length = 100)
+    currency = models.CharField(max_length = 20)
+    timeOpen = models.CharField(max_length = 20)
+    timeClose = models.CharField(max_length = 20)
+    timeZone = models.CharField(max_length = 20)
+    marketCap = models.FloatField()
+    price = models.FloatField()
+    changePercent = models.FloatField()
+    data = models.CharField(max_length = 100)
+    userId = models.IntegerField(default= 0)
--- src/carrinho/views.py
+++ src/carrinho/views.py
@@ -1,20 +1,49 @@
 from django.shortcuts import render
+from .models import Investimento
 
 def Listacarrinho(request):
-    context = {}
+    context ={}
     try:
-        # ta funcionando
-        # 
-        context['acoes'] = request.session['acoes']
+        listaInvestimentos = []
+        listaInvestimentos = request.session['acoes']
+        context['acoes'] = listaInvestimentos
         print('---------carrinho------------')
-        print(request.session['acoes'])
+        #print(request.session['acoes'])
         print('----------carrinho-----------')
-
     except:
         context['vazio'] = True
-        print('erro')
-   
+        print('session vazia')
 
-    return render(request, 'index_carrinho.html', context )
+    if request.method == 'POST':
+        if request.POST.get('investir') == 'investir':
+            userId = 0
+            if request.user.is_authenticated:
+                userId = request.user.id
+            
+            for i in listaInvestimentos:
+                Investimento(
+                    symbol = i['symbol'],
+                    name= i['name'],
+                    region = i['region'],
+                    currency = i['currency'],
+                    timeOpen= i['time_open'],
+                    timeClose= i['time_close'],
+                    timeZone= i['timezone'],
+                    marketCap=i['market_cap'],
+                    price= i['price'],
+                    changePercent= i['change_percent'],
+                    data= i['updated_at'],
+                    userId = userId,
+                ).save()
+            request.session['acoes'] = []
+            print('-------------investiu------------------')
+            return render(request, 'index_carrinho.html', context )
+        elif request.POST.get('excluir') == 'excluir':
+            request.session['acoes'] = []
+            print('-------------excluiu------------------')
+            return render(request, 'index_carrinho.html', context )
+        return render(request, 'index_carrinho.html', context )
+    else: 
+        return render(request, 'index_carrinho.html', context )
 
     
